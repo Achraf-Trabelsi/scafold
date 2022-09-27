9f58f3273207ba9bab14c3da5635ee25e8859504
achref Trabelsi <achref.trabelsi.insat@gmail.com>
2022-09-27 10:35:34 +0100

    added multiply

--- hello.py
+++ hello.py
@@ -2,4 +2,9 @@ def add(x, y):
     return x + y
 
 
+def multiply(x, y):
+    return x * y
+
+
 print(f"this is the sum: 1, 2, {add(1, 2)}")
+print(f"this is the sum: 1, 2, {multiply(1, 2)}")
--- test_hello.py
+++ test_hello.py
@@ -1,5 +1,9 @@
-from hello import add
+from hello import add, multiply
 
 
 def test_add():
     assert add(1, 2) == 3
+
+
+def test_mutilply():
+    assert multiply(1, 2) == 2
